18842a9c2e2ea7ddc50a501a7d5f45bb9a8b307a
daffuwu <dananda41661@gmail.com>
2026-02-11 23:59:58 +0700

    Reformat whole codebase

--- src/main/java/com/example/individualprep/service/ArithmeticUtility.java
+++ src/main/java/com/example/individualprep/service/ArithmeticUtility.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ArithmeticUtility {
-    
+
     public double add(final double o1, final double o2) {
-        return o1+o2;
+        return o1 + o2;
     }
 
     public double subtract(double o1, double o2) {
