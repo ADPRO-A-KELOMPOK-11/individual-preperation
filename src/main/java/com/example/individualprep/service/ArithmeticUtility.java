1bbdbb3d54d9e5e80690ff1a01a2692c7cdb07dd
alvinchristian <alvch6@gmail.com>
2026-02-11 21:43:30 +0700

    (chore): fix indent issues

--- src/main/java/com/example/individualprep/service/ArithmeticUtility.java
+++ src/main/java/com/example/individualprep/service/ArithmeticUtility.java
@@ -4,27 +4,27 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ArithmeticUtility {
-    public double add(double o1, double o2) {
-        // TODO: Implement me properly!
-        return 0.0;
-    }
+  public double add(double o1, double o2) {
+    // TODO: Implement me properly!
+    return 0.0;
+  }
 
-    public double subtract(double o1, double o2) {
-        // TODO: Implement me properly!
-        return 0.0;
-    }
+  public double subtract(double o1, double o2) {
+    // TODO: Implement me properly!
+    return 0.0;
+  }
 
-    public double multiply(final double o1, final double o2) {
-        return o2 * o1;
-    }
+  public double multiply(final double o1, final double o2) {
+    return o2 * o1;
+  }
 
-    public double divide(double o1, double o2) {
-        // TODO: Implement me properly!
-        return 0.0;
-    }
+  public double divide(double o1, double o2) {
+    // TODO: Implement me properly!
+    return 0.0;
+  }
 
-    public double exponent(double o1, int n) {
-        // TODO: Implement me properly!
-        return 0.0;
-    }
+  public double exponent(double o1, int n) {
+    // TODO: Implement me properly!
+    return 0.0;
+  }
 }
